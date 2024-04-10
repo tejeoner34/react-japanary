bd204ea3de4eb2f7d8e2f2fda4d0e2cfed35a636
tejeoner34 <atejezar@gmail.com>
2024-04-10 20:45:41 +0200

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: {
+        100: '#dce6ea',
+        200: '#b8ccd6',
+        300: '#95b3c1',
+        400: '#7199ad',
+        500: '#4e8098',
+        600: '#3e667a',
+        700: '#2f4d5b',
+        800: '#1f333d',
+        900: '#101a1e',
+      },
+      primaryText: '#101a1e',
+      secondaryText: '#7199ad',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],

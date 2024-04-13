65f16501aeed0978922c02671d777778153de675
tejeoner34 <atzarco37@gmail.com>
2024-04-13 19:44:29 +0200

    create router and auth screens

--- src/services/dictionary.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'http://127.0.0.1:5000/';
-export async function searchWord(word: string) {
-  const response = await axios.get(`${BASE_URL}?keyword=${word}`);
-  console.log(response);
-  return response.data;
-}

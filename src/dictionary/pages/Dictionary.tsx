6e9cf5d27d67da27ba970413a7827cb3369b1f70
tejeoner34 <atejezar@gmail.com>
2024-07-13 19:02:48 +0200

    add furigana, styles and fixed search button

--- src/dictionary/pages/Dictionary.tsx
+++ src/dictionary/pages/Dictionary.tsx
@@ -2,6 +2,7 @@ import Form from '@/dictionary/components/Form';
 import SearchResultsList from '@/dictionary/components/SearchResultsList';
 import { useDictionary } from '@/dictionary/hooks/useDictionary';
 import SampleSentenceSection from '../components/SampleSentenceSection';
+import { Button } from '@/common/components/ui';
 
 export default function DictionaryScreen() {
   const { searchWord, searchSampleSenteces, sampleSentences, searchedWordResult } = useDictionary();
@@ -11,13 +12,20 @@ export default function DictionaryScreen() {
   };
   return (
     <div className="container grid place-items-center gap-4 p-5">
-      <div className="max-w-md w-full">
+      <div className="bg-backgroundSecondary max-w-md w-full">
         <Form onSubmit={handleSearch} />
       </div>
 
-      <div className="w-full contents">
-        <SearchResultsList searchResultsList={searchedWordResult} />
-        <SampleSentenceSection exampleSentences={sampleSentences} />
+      <div className="flex flex-col gap-5 w-full md:flex-row">
+        <div className="bg-backgroundSecondary h-fit">
+          <SearchResultsList searchResultsList={searchedWordResult} />
+        </div>
+        <div className="max-w-md  bg-backgroundSecondary">
+          <SampleSentenceSection exampleSentences={sampleSentences} />
+        </div>
+      </div>
+      <div className="fixed bottom-10 md:invisible ">
+        <Button>+</Button>
       </div>
     </div>
   );

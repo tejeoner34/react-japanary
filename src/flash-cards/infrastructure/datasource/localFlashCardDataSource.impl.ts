0499dd4c59a06b6ddae87297de0029f7a37fc479
tejeoner34 <atejezar@gmail.com>
2024-07-16 20:26:37 +0200

    add deck model and use case definition

--- src/flash-cards/infrastructure/datasource/localFlashCardDataSource.impl.ts
+++ src/flash-cards/infrastructure/datasource/localFlashCardDataSource.impl.ts
@@ -1,6 +1,7 @@
 import { FlashCardDataSource } from '@/flash-cards/domain/datasource/flashCardDataSource';
 import { FlashCard, Grade } from '@/flash-cards/domain/models/flashCards.model';
 import { SpaceRepetition } from '../helpers/spaceRepetition';
+import { Deck } from '@/flash-cards/domain/models/deck.model';
 
 export class LocalFlashCardDataSourceImpl implements FlashCardDataSource {
   createFlashCard(flashCard: FlashCard): void {
@@ -22,6 +23,18 @@ export class LocalFlashCardDataSourceImpl implements FlashCardDataSource {
   }
 
   deleteFlashCard(flashCard: FlashCard): void {
+    throw new Error('Method not implemented.');
+  }
+
+  createDeck(deck: Deck): Deck[] {
+    throw new Error('Method not implemented.');
+  }
+
+  editDeck(deck: Deck): Deck[] {
+    throw new Error('Method not implemented.');
+  }
+
+  deleteDeck(deck: Deck): Deck[] {
     throw new Error('Method not implemented.');
   }
 }

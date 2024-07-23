ad29ac83d885745202a93197326d6ad561c817cf
tejeoner34 <atejezar@gmail.com>
2024-07-23 20:21:55 +0200

    add msw dependency for mocking http request in tests

---
+++ tests/mocks/dictionaryHandlers.ts
@@ -0,0 +1,13 @@
+import { SAMPLE_SENTENCES_MOCK, WORD_QUERY_MOCK } from '@/dictionary/mocks/dictionary.mocks';
+import { http, HttpResponse } from 'msw';
+
+const BASE_URL: string = import.meta.env.VITE_DICTIONARY_BASE_URL;
+
+export const dictionaryHanlders = [
+  http.get(BASE_URL, () => {
+    return HttpResponse.json(WORD_QUERY_MOCK);
+  }),
+  http.get(`${BASE_URL}/sample-sentence`, () => {
+    return HttpResponse.json(SAMPLE_SENTENCES_MOCK);
+  }),
+];

f4134b98480aebaf89b3906c9d05a5dac91c46e0
tejeoner34 <atejezar@gmail.com>
2024-08-10 18:52:26 +0200

    feat: add custom drop down menu

--- src/flash-cards/pages/DecksPage.tsx
+++ src/flash-cards/pages/DecksPage.tsx
@@ -1,17 +1,32 @@
 import CustomText from '@/common/components/ui/CustomText';
 import { useFlashCard } from '../hooks/useFlashCard';
 import { Button } from '@/common/components/ui';
-import { Plus } from 'lucide-react';
+import { Folders, CreditCard, Plus } from 'lucide-react';
+import CustomDropdownMenu from '@/common/components/ui/CustomDropdownMenu';
+
+const dropdownMenuItems = [
+  {
+    name: 'Create new deck',
+    icon: <Folders className="mr-2 h-4 w-4" />,
+  },
+  {
+    name: 'Create new flash card',
+    icon: <CreditCard className="mr-2 h-4 w-4" />,
+  },
+];
 
 export default function DecksPage() {
   const { flashCards } = useFlashCard();
   return (
     <>
       <CustomText tag="h1" text="Your decks" />
+
       <div className="flex justify-end z-100 fixed bottom-0 p-6 w-full">
-        <Button className="h-16 w-16" variant="secondaryShadow">
-          <Plus className="h-5 w-5" />
-        </Button>
+        <CustomDropdownMenu items={dropdownMenuItems}>
+          <Button className="h-16 w-16" variant="secondaryShadow">
+            <Plus className="h-5 w-5" />
+          </Button>
+        </CustomDropdownMenu>
       </div>
     </>
   );

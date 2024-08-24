6e5b422b96c2c175b149c6707a212ebc32eee0ea
tejeoner34 <atzarco37@gmail.com>
2024-08-24 12:30:23 +0200

    fix: unit test errors

--- tests/config/contextProviders.tsx
+++ tests/config/contextProviders.tsx
@@ -1,10 +1,18 @@
 import DictionaryContextProvider from '@/dictionary/context/dictionaryContext';
+import { FlashCardsContextProvider } from '@/flash-cards/context/flashCardsContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MemoryRouter } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 export const AllProviders: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <MemoryRouter>
-      <DictionaryContextProvider>{children}</DictionaryContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <FlashCardsContextProvider>
+          <DictionaryContextProvider>{children}</DictionaryContextProvider>
+        </FlashCardsContextProvider>
+      </QueryClientProvider>
     </MemoryRouter>
   );
 };
--- tests/mocks/dictionaryHandlers.ts
+++ tests/mocks/dictionaryHandlers.ts
@@ -1,10 +1,11 @@
 import { SAMPLE_SENTENCES_MOCK, WORD_QUERY_MOCK } from '@/dictionary/mocks/dictionary.mocks';
 import { http, HttpResponse } from 'msw';
 
-const BASE_URL: string = import.meta.env.VITE_DICTIONARY_BASE_URL;
+const BASE_URL: string = import.meta.env.VITE_DICTIONARY_BASE_URL + 'dictionary';
 
 export const dictionaryHanlders = [
   http.get(BASE_URL, () => {
+    console.log('ASDADSADSADSADSADSADASDAS');
     return HttpResponse.json(WORD_QUERY_MOCK);
   }),
   http.get(`${BASE_URL}/sample-sentence`, () => {

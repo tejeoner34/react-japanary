6fec7dc8fc49ceda3e5068fb0349d19bba0dd543
tejeoner34 <atzarco37@gmail.com>
2024-09-01 13:59:50 +0200

    fix: remove github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    test: {
-      environment: 'jsdom',
-      globals: true,
-      setupFiles: 'tests/config/setup.ts',
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: 'tests/config/setup.ts',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    plugins: [react()],
-    base: '/',
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-  };
-  if (command !== 'serve') config.base = '/react-japanary/';
-  return config;
+  },
 });

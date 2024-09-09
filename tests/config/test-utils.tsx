5c0063c5f8ec7634deb45beb8d201d703a67bb5f
tejeoner34 <atejezar@gmail.com>
2024-09-09 16:42:20 +0200

    feat: cancel http request

--- tests/config/test-utils.tsx
+++ tests/config/test-utils.tsx
@@ -1,12 +1,24 @@
 /* eslint-disable react-refresh/only-export-components */
 
-import { ReactElement } from 'react';
-import { render, RenderOptions } from '@testing-library/react';
+import { ReactElement, ReactNode } from 'react';
+import { render, renderHook, RenderHookOptions, RenderOptions } from '@testing-library/react';
 import { AllProviders } from './contextProviders';
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
   render(ui, { wrapper: AllProviders, ...options });
 
+const customRenderHook = <TProps, TResult>(
+  hook: (props: TProps) => TResult,
+  options?: RenderHookOptions<TProps>
+) => {
+  const wrapper = ({ children }: { children: ReactNode }) => (
+    <AllProviders>{children}</AllProviders>
+  );
+
+  return renderHook(hook, { wrapper, ...options });
+};
+
 export * from '@testing-library/react';
 
 export { customRender as render };
+export { customRenderHook as renderHook };

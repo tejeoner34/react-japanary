ee7ef7872869e7eee970818b7534c3add0640dcd
tejeoner34 <atejezar@gmail.com>
2024-12-30 17:59:30 +0100

    fix: add searchAi method to interface impl

--- src/dictionary/infrastructure/datas-sources/dictionaryDSMockImpl.ts
+++ src/dictionary/infrastructure/datas-sources/dictionaryDSMockImpl.ts
@@ -1,8 +1,12 @@
-import { ExampleSentence, SearchResult } from '@/dictionary/models/searchResult';
+import { AiResponse, ExampleSentence, SearchResult } from '@/dictionary/models/searchResult';
 import { DictionaryDataSource } from './dictionaryDataSource';
 import { SAMPLE_SENTENCES_MOCK, WORD_QUERY_MOCK } from '@/dictionary/mocks/dictionary.mocks';
 
 export class DictionaryDSMockImpl implements DictionaryDataSource {
+  searchAi(word: string): Promise<AiResponse> {
+    console.log(word);
+    throw new Error('Method not implemented.');
+  }
   searchSampleSenteces(word: string): Promise<ExampleSentence[]> {
     console.log(word);
     return new Promise((resolve) => {

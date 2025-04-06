4c8ae8db2dcba0b217bf6cd90d8c8a704e396d31
tejeoner34 <atejezar@gmail.com>
2025-04-06 20:23:59 +0900

    fix: study page bug when there are no pending study cards

--- src/flash-cards/pages/StudyPage.tsx
+++ src/flash-cards/pages/StudyPage.tsx
@@ -1,13 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useFlashCardsContext } from '../hooks/useFlashCardsContext';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import StudyFlashCard from '../components/ui/StudyFlashCard.component';
 
 export default function StudyPage() {
+  const navigate = useNavigate();
   const { deckId } = useParams();
   const { decks } = useFlashCardsContext();
   const deck = useMemo(() => decks.find((deck) => deck.id === deckId), [deckId, decks]);
+
+  useEffect(() => {
+    if (deck && deck.cards.pendingStudyAmount <= 0) {
+      navigate('/decks');
+    }
+  }, [deck, navigate]);
+
   if (!deck) return <div>Deck not found</div>;
+  if (deck?.cards.pendingStudyAmount <= 0) return null;
+
   return (
     <>
       <StudyFlashCard cardsToStudy={deck.cards.pedingStudyCards} />

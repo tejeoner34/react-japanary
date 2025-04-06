c6dafece8d6ce4f6ee9de17ed822c4181551e633
tejeoner34 <atejezar@gmail.com>
2025-04-06 13:44:18 +0900

    feat: add line break in flashcard form

--- src/flash-cards/components/ui/FlashCardForm.tsx
+++ src/flash-cards/components/ui/FlashCardForm.tsx
@@ -140,7 +140,7 @@ export default function FlashCardForm({
   useEffect(() => {
     setForm({
       ...form,
-      back: flashCardToEdit?.back + aiResponse,
+      back: flashCardToEdit?.back + '\n' + aiResponse,
     });
   }, [aiResponse]);
 
